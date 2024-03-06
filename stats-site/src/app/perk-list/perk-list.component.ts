7894294c00f5d79475946913730f72e424d2159b
lybned <nedliu2001@gmail.com>
2024-03-06 15:47:34 -0700

    Changed Perk Description
    1. Make sure descriptions are in HTML text
    
    2. Make sure value in the descriptions are updated based on the input array.

--- stats-site/src/app/perk-list/perk-list.component.ts
+++ stats-site/src/app/perk-list/perk-list.component.ts
@@ -36,9 +36,46 @@ export class PerkListComponent implements OnInit {
     // Modify perk.img here as needed
     // Example: You can append a suffix to the image filename
     let temp = perk.split("_")[1]
-    console.log("temp",temp)
+    //console.log("temp",temp)
     return "assets/icons/iconPerks_" + this.capitalizeFirstLetter(temp)
   }
+
+  colorLevelText(text: string, level:number) : string{
+    if (level === 1){
+      return `<span class="text-yellow-400 font-bold">${text}</span>`
+    } else if (level === 2){
+      return `<span class="text-green-500 font-bold">${text}</span>`
+    } else if (level === 3){
+      return `<span class="text-violet-500 font-bold">${text}</span>`
+    } else {
+      return `<span class="font-bold">${text}</span>`
+    }
+  }
+
+  getFillInDescription(perk: Perk): string{
+
+    let description = perk.description.split("<br><br><br>").join("<br><br>");
+    let cont = true
+    let counter = 0
+    while (cont){
+      const pattern = ('{' + counter + '}')
+      if ( description.includes(pattern) ){
+        if (perk.tunables[counter].length > 1){
+          const param = perk.tunables[counter].map( (x,i) => this.colorLevelText(x.toString(),(i+1)) ).join();
+          description = description.replace(pattern, param)          
+        } else {
+          const param = perk.tunables[counter].map( (x,i) => this.colorLevelText(x.toString(),0) ).join();
+          description = description.replace(pattern, param)                  
+        }
+        counter += 1
+      } else {
+        cont =  false
+      }
+    }
+    //console.log(description)
+    return description
+  }
+
 
 
 }
