b87fd73e6b8177443b3e9238c1fd7ef5213e36ff
lybned <nedliu2001@gmail.com>
2024-05-03 17:56:40 -0600

    Added Home Page and Logo

--- stats-site/src/app/app.routes.ts
+++ stats-site/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PerkListComponent } from './perk-list/perk-list.component';
 import { EventsComponent } from './events/events.component';
+import { HomePageComponentComponent } from './home-page-component/home-page-component.component';
 export const routes: Routes = [
   { path:'AllPerks', component:PerkListComponent},
-  { path:'Events', component:EventsComponent}
+  { path:'Events', component:EventsComponent},
+  { path:'', component:HomePageComponentComponent}
 ];
